4c0d1271f88f381f93320f9c4bda0253e4b3366b
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-08-20 05:24:18 -0300

    fix: downgrade to v3.6.10

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff2bd0c3c55701062fdb61b26a6ab685'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2f567922-88db-4dea-9f7b-6dee3374ac9a'
+};
